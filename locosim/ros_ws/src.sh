14cb285dcaf91c2f6630829f196ec9afdb2c1940
Kocsis Dávid <kocsis.david@edu.bme.hu>
2025-12-04 16:12:45 +0100

    Packagesorurce

--- locosim/ros_ws/src.sh
+++ locosim/ros_ws/src.sh
@@ -1,4 +1,6 @@
 source /opt/ros/noetic/setup.bash
 source /home/ubuntu/ros_ws/devel/setup.bash
 export PYTHONPATH=$PYTHONPATH:/opt/ros/noetic/lib/python3/dist-packages
+export PYTHONPATH=$PYTHONPATH:/opt/ros/noetic/lib/python3.8/site-packages
 roscore
+#Ex python3 -i $LOCOSIM_DIR/robot_control/base_controllers/base_controller.py
